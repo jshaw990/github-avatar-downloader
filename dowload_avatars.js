7e6b9cec413d02a74ea57ad3997c86d5adb975f7
Jayden Shaw <jayden.shaw@gmail.com>
2018-10-29 23:14:49 +0000

    secrets shhh

--- dowload_avatars.js
+++ dowload_avatars.js
@@ -1,2 +1,15 @@
 var request = require('request'); 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner+ "/" + repoName + "/contributors", 
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    
+    request(url, function(err, result, body) {
+    cb(err, body);
+    });
+}
