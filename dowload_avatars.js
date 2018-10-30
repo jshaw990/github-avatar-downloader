a73f0a28dc5e4f09678ca4b308668576c6ca4dc6
Jayden Shaw <jayden.shaw@gmail.com>
2018-10-30 00:06:54 +0000

    Almost there

--- dowload_avatars.js
+++ dowload_avatars.js
@@ -1,32 +1,46 @@
+//Required Modules
 var request = require('request'); 
+var secret = require("./secrets");
+var fs = require("fs");
+
+//Welome Message
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//Fetches Repo Information
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner+ "/" + repoName + "/contributors", 
         headers: {
-            'User-Agent': 'request'
+            'User-Agent': 'request', 
+            'Authorization' : secret.GITHUB_TOKEN
         }
     };
     
-    request(url, function(err, result, body) {
+    request(options, function(err, result, body) {
     cb(err, body);
     });
 }
 
+//Parses URLs and isolates Avatars
 function isoAvatar(err, data) {
     if (err) {
         console.log("Error: " + err);
     } else {
         console.log("Avatars Downloading...");
         JSON.parse(data).forEach (user => {
-            downloadURL(user.avatar_url,"avatars/" + user.login + ".jpg");
+            downloadImageByURL(user.avatar_url,"./Avatars/" + user.login + ".jpg");
         });
     }
 
 }
 
+//Downloads Avatars to user
 function downloadImageByURL(url, filePath) {
     request(url)
         .pipe(fs.createWriteSteam(filepath)); 
 }
+
+getRepoContributors();
+
+console.log ("Process Complete");
+
